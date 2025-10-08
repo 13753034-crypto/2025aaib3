082fe8b2e19e580cf7fb5ec4e5fdfcf1056c939c
13753034-crypto <13753034@me.mcu.edu.tw>
2025-10-08 11:41:24 +0800

    week05

---
+++ week05/week05-3a.cpp
@@ -0,0 +1,15 @@
+/// week05-3a.cpp
+/// CPE 這次的第2題 UVA 483 Word Scramble 把字弄反
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main()
+{
+    string line; /// Part 1: Input
+    while (getline (cin, line)){ /// 讀入1行
+
+        /// Part 2: Output
+        cout << line << endl; /// 先隨便印出來
+    }
+}
